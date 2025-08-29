f12dd46f25eb48379b47967920a5d5d904e327fb
Materac <materacwspolpraca@gmail.com>
2025-08-29 14:21:28 +0200

    First Update, Created raport Function

--- makefile
+++ makefile
@@ -1,3 +1,8 @@
-default: normal
-normal:
+#default: normal
+build:
 	gcc src/task.c -o task.x -lreadline -lhistory -lncurses
+install:
+	echo sudo install -Dm755 task.x /bin/luna-taskmanctl
+	@ echo To use this program use comand \"luna-taskmanctl\"
+test:
+	io || echo 123
--- src/task.c
+++ src/task.c
@@ -32,24 +32,30 @@ char *input = NULL;
 char *inputf = NULL;
 char *inputb = NULL;
 char *sp = NULL;
+char *group = NULL;
 char path[512];
 char pathtask[512];
 char pathconfig[512];
 char lastinput[512];
 DIR *d;
+struct dirent *entry;
+struct stat st;
 
 void handle_kill(int sig) {
 	if (input) { free(input); input = NULL; }
     if (inputf) { free(inputf); inputf = NULL; }
     if (inputb) { free(inputb); inputb = NULL; }
 	if (sp) { free(sp); sp = NULL; }
+	if (group) { free(group); group = NULL; }
+	if (d) closedir(d);
 	write_history(path);
 	closedir(d);
 	printf("\n");
 	exit(0);
 }
 
-int main(int argc, char *argv[]) {	
+int main(int argc, char *argv[]) {
+	group = strdup("none");
 	char *home = getenv("HOME");
 	snprintf(pathtask, sizeof(pathtask), "%s/.task", home);
 	snprintf(pathconfig, sizeof(pathconfig), "%s/.task/config.json", home);
@@ -60,13 +66,33 @@ int main(int argc, char *argv[]) {
 	if (access(pathconfig, F_OK) != 0) {
 		system("touch ~/.task/config.json");
 		system("echo '{\n	\"cat\": \"glow --pager\",\n	\"edit\": \"nano\"\n}' >> ~/.task/config.json");
+		printf("exit - to exit ctl\naddgrp foo - to add new group named foo\ndelgrp foo - to delete group foo\nchgrp foo - changes working group to foo\naddtask foo - to add new task named foo in working group\ndeltask foo - to delete task foo in working group\ninfotask foo - shows information about task foo in working group\nmodtask foo - edits informations about task foo in working group\ndonetask foo - flags task foo as done\ninworktask foo - flags task foo as in work\nundownetask foo - flags task foo as un done\nlsgrp - list all groups\nlstask - list all task in working group\nconfig - alows to configure programs to open and edit tasks\nhelp - shows this menu\n");
 	}
 	if (argc >= 2) {
 		if (strcmp(argv[1], "raport") == 0) {
-			printf("Raportuje!\n");
-			if (d) {
-				printf("Działa Dir: %s \n", pathtask);
+			while ((entry = readdir(d)) != NULL) {
+				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+					continue;
+				char fullpath[1024];
+				snprintf(fullpath, sizeof(fullpath), "%s/%s", pathtask, entry->d_name);
+				if (stat(fullpath, &st) == 0 && S_ISDIR(st.st_mode)) {
+					printf("  Group: %s\n", entry->d_name);
+					char todo[1024]; 
+					snprintf(todo, sizeof(todo), "find '%s/.task/%s/todo' -maxdepth 1 -type f -name '*.md' -printf '%%f\\n' | sed 's/\\.md$//'", home, entry->d_name);
+					printf(" To Do: \n");
+					system(todo);
+					char inwork[1024];
+					snprintf(inwork, sizeof(inwork), "find '%s/.task/%s/inwork' -maxdepth 1 -type f -name '*.md' -printf '%%f\\n' | sed 's/\\.md$//'", home, entry->d_name);
+					printf(" In Work: \n");
+					system(inwork);
+				}
 			}
+			if (d) closedir(d);
+			if (input) { free(input); input = NULL; }
+			if (inputf) { free(inputf); inputf = NULL; }
+			if (inputb) { free(inputb); inputb = NULL; }
+			if (sp) { free(sp); sp = NULL; }
+			if (group) { free(group); group = NULL; }
 			return 0;
 		}
 	}
@@ -75,7 +101,9 @@ int main(int argc, char *argv[]) {
 	signal(SIGTERM, handle_kill);
 	signal(SIGINT, handle_kill);
 	while (1) {
-		input = readline("> ");
+		char prompt[100];
+		snprintf(prompt, sizeof(prompt), "Group: %s >>> ", group);
+		input = readline(prompt);
 		if (history_length > 0) {
 			const char *line = history_get(history_length)->line;
 			strncpy(lastinput, line, sizeof(lastinput));
@@ -98,13 +126,53 @@ int main(int argc, char *argv[]) {
 		//strncpy(lastinput, input, sizeof(lastinput));
 		//lastinput[sizeof(lastinput) - 1] = '\0';	
 		if (strcmp(inputf, "exit") == 0 || strcmp(inputf, "quit") == 0) {
-			if (sp) free(sp);
-			if (input) free(input);
-			if (inputb) free(inputb);
-			if (inputf) free(inputf);
+			if (sp) { free(sp); }
+			if (input) { free(input); }
+			if (inputf) { free(inputf); }
+			if (inputb) { free(inputb); }
+			if (group) { free(group); }
 			write_history(path);
 			if (d) closedir(d);
 			break;
+		} else if (strcmp(inputf, "addgrp") == 0) {
+			if (inputb) {
+				char tmp[512];
+				snprintf(tmp, sizeof(tmp), "mkdir -p ~/.task/%s", inputb);
+				system(tmp);
+				snprintf(tmp, sizeof(tmp), "mkdir -p ~/.task/%s/todo", inputb);
+				system(tmp);
+				snprintf(tmp, sizeof(tmp), "mkdir -p ~/.task/%s/inwork", inputb);
+				system(tmp);
+				snprintf(tmp, sizeof(tmp), "mkdir -p ~/.task/%s/done", inputb);
+				system(tmp);
+			} else {
+				printf("%s requires an argument\n", inputf);
+			}
+		} else if (strcmp(inputf, "delgrp") == 0) {
+			if (inputb) {
+				char tmp[512];
+				snprintf(tmp, sizeof(tmp), "rm -rf ~/.task/%s", inputb);
+				system(tmp);
+				if (strcmp(inputb, group) == 0) {
+					group = "none";
+				}
+			} else {
+				printf("%s requires an argument\n", inputf);
+			}
+		} else if (strcmp(inputf, "chgrp") == 0) {
+			if (inputb) {
+				char tmp[512];
+				snprintf(tmp, sizeof(tmp), "%s/.task/%s", home, inputb);
+				if (access(tmp, F_OK) == 0) { 
+					//strcpy(group, inputb);
+					free(group);
+					group = strdup(inputb);
+				} else {
+					printf("Group %s dosen\'t exist\n", inputb);
+				}
+			} else {
+				printf("%s requires an argument\n", inputf);
+			}
 		}
 	}
 	return 0;
